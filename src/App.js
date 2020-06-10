c67a91c2f731dd2f680ed1041b4dbe74b13e5b06
Rafiya Mateen <r.mateen666@gmail.com>
2020-06-11 01:40:22 +0500

    Made an increase-decrease counter and added dark mode and light mode option

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Counter } from './Counter.js';
 import './App.css';
 
 function App() {
+  let [count, setCount] = useState(0);
+  let [darkMode, setMode] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className={`counter ${darkMode ? 'dark' : ''}`}>
+      <h1>{darkMode ? 'Dark' : 'Light'} Mode</h1>
+      <button onClick={()=>setMode(!darkMode)}>Change mode</button>
+    <Counter value={count} />
+    <button onClick={() => setCount(--count)}>Decrease</button>
+    <button onClick={() => setCount(++count)}>Increase</button>
+    </div >
   );
 }
 
---
+++ src/Counter.js
@@ -0,0 +1,6 @@
+import React from 'react';
+export function Counter (props){
+    return(
+        <h2>Counter value: {props.value}</h2>
+    )
+}
